dadf08af3406600b98ecf05ee1c8cedf5fe83206
Uliana Mytlovych <ulianamytlovych@gmail.com>
2024-09-27 17:17:31 +0300

    add file main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello world")
+num1 =input("Enter first number:")
+num2 =input("Enter second number:")
+
+sum =float(num1)+float(num2)
+#Вивести суму
+print("The sum of{0} and {1} is {2}" format(num1,num2,sum))
+
+
